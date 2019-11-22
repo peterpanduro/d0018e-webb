c9109e175b5481433ae3e83b3f3f52788b3ddbb0
Peter Panduro <peter@kodifiera.se>
2019-11-22 17:47:07 +0100

    Added React router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
+import Home from './views/Home'
+import Products from './views/Products'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav className="Navbar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">Produkter</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <body>
+          <Switch>
+            <Route path="/products">
+              <Products />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </body>
+      </div>
+    </Router>
   );
 }
 
