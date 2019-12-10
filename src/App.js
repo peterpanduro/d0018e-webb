8f8e829decdc9c8d51827c5561b647419d099e80
Peter Panduro <peter@kodifiera.se>
2019-12-10 22:57:57 +0100

    More refacroting and cleaner code

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './views/Home'
 import Products from './views/Products'
 import Login from './views/Login'
 import RegisterProfile from './views/RegisterProfile'
-import SearchBox from './views/components/SearchBox'
-import ProductList from './views/components/ProductList';
+import Header from './views/components/Header'
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <nav className="Navbar">
-              <ul>
-                <li><SearchBox/></li>
-                <li>
-                  <Link to="/">Startsida</Link>
-                </li>
-                <li>
-                  <Link to="/products">Produkter</Link>
-                </li>
-                <li>
-                  <Link to="/login">Logga in</Link>
-                </li>
-                <li>
-                  <Link to="/register">Registrera profil</Link>
-                </li>
-              
-            </ul>
-          </nav>
-          
-        </header>
-        
-        <body>
-          <div className ="Background">
-            <div className = "Content">
-
-              <Switch>
-                <Route path="/products/category/:category_id" component={Products} />
-
-                <Route exact path="/products">
-                  <Products />
-                </Route>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path ="/login">
-                  <Login />
-                </Route>
-                <Route exact path ="/register">
-                  <RegisterProfile />
-                </Route> 
-                <Route exact path ="/category/:id">
-                    <RegisterProfile />
-                </Route>
-              </Switch>
-            </div>
+        <Header />
+        <div className ="Background">
+          <div className = "Content">
+            <Switch>
+              <Route exact path="/products/category/:category_id" component={Products} />
+              <Route exact path="/products" component={Products} />
+              <Route exact path="/" component={Home} />
+              <Route exact path ="/login" component={Home} />
+              <Route exact path ="/register" component={RegisterProfile} />
+            </Switch>
           </div>
-
-           
-        </body>
+        </div>           
       </div>
     </Router>
   );
