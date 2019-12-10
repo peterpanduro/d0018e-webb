6a8b96e35deae5d60f88d986962d16c1e993c612
magdulator <magdaa.a@hotmail.com>
2019-12-10 11:31:54 +0100

    login woho

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Login.css';
+
 
 function Login() {
   return (
     <div className="Login">
-      <h1>Logga in</h1>
-      
+        <div className = "container">
+            <div className = "otherBox">
+                <h1>Logga in på datamerchstore AB</h1>
+                <h3>Med ett konto på datamerchstore AB kan du göra allt detta:</h3><br/>
+                <ul>
+                    <li>Spara din address</li>
+                    <li>Snabbare checkout</li>
+                    <li>Spara din kundkorg</li>
+                    <li>Få sålda uppgifter</li>
+                </ul>
+            </div>
+
+            <div className = "loginBox">
+                <h2>Logga in</h2>
+                <div className = "input-container">
+                    <h3>E-post address</h3>
+                    <form action ="google.com">
+                        <input type="text" name="namn"></input>
+                    </form>
+                    <h3>Lösenord</h3>
+                    <form action ="google.com">
+                        <input type="text" name="password"></input>
+                    </form>
+                </div>
+            </div>
+
+            
+        </div>
     </div>
   );
 }
