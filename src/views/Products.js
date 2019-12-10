901835b31dc24ace9329acd0fa2cbf315e773914
magdulator <magdaa.a@hotmail.com>
2019-12-10 14:32:48 +0100

    products blabla

--- src/views/Products.js
+++ src/views/Products.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Categories from './components/CategoryList';
+import ProductList from './components/ProductList';
+import './Products.css';
+
 
 function Products() {
   return (
     <div className="Products">
-      <br></br>
-      <Categories/>
+      <br/>
+      
+      <Categories/><br/>
+      <div className = "container2">
+        <div className = "border">
+
+          <p>Populära varor</p>
+        </div>
+        <ProductList/><br/>
+      </div>
     </div>
   );
 }
