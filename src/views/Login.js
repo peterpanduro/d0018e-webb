15b474ff5a9b1950d5862683ba579784bad951b9
magdulator <magdaa.a@hotmail.com>
2019-12-12 15:26:23 +0100

    hilfe schlangeman

--- src/views/Login.js
+++ src/views/Login.js
@@ -3,46 +3,80 @@ import './Login.css';
 
 
 
-function Login() {
-  return (
-    
-    <div className="Login">
-        <div className = "container">
-            <div className = "otherBox">
-                <h1>Logga in på datamerchstore AB</h1>
-                <h3>Med ett konto på datamerchstore AB kan du göra allt detta:</h3><br/>
-                <ul>
-                    <li>Spara din address</li> <br/> 
-                    <li>Snabbare checkout</li><br/>
-               
-                    <li>Spara din kundkorg</li><br/>
-                    <li>Få sålda uppgifter</li>
-                </ul>
-            </div>
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.submitHandler = this.submitHandler.bind(this);
+        this.handler = this.handler.bind(this);
+    }
 
-            <div className = "loginBox">
-                <h2>Logga in</h2>
-                <div className = "input-container">
-                    
-                    <form action="www.google.com">
-                        <h3>E-post address</h3>
-                        <input type="text" name="Email"></input>
-                        
-                        <h3>Lösenord</h3>
-                        <input type="text" name="Password"></input>
-  
-                        <input type="submit" value="Logga in"></input>
-                    </form> 
+    submitHandler = e => {
+        this.handler(e); 
+     }
 
+    updateEmail = e => {
+        this.setState({email: e.target.value});
+    }
 
-                </div>
-            </div>
+    updatePassword = e => {
+        this.setState({password: e.target.value});  
+    }
 
+    async handler(e) {
+        e.preventDefault();
+        fetch('http://api.d0018e.pndro.se/user/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'email': this.state.email,
+                'password': this.state.password
+            }
             
-        </div>
-    </div>
-    
-  );
-}
+        }).then(response => {
+            response.json().then(json => {
+                console.log(json);
+                console.log(response.status);
+                console.log(this.state.email);
+            })
+        })
+    }
 
+    render() {
+        return (
+            
+            <div className="TestyTest">
+            <div className = "container">
+                <div className = "otherBox">
+                    <h1>Logga in på datamerchstore AB</h1>
+                    <h3>Med ett konto på datamerchstore AB kan du göra allt detta:</h3><br/>
+                    <ul>
+                        <li>Spara din address</li> <br/> 
+                        <li>Snabbare checkout</li><br/>
+                   
+                        <li>Spara din kundkorg</li><br/>
+                        <li>Få sålda uppgifter</li>
+                    </ul>
+                </div>
+                <div className = "loginBox">
+                    <h2>Logga in</h2>
+                    <div className = "input-container">
+                        <form onSubmit={this.handler}>
+                            <h3>E-post address</h3>
+                            <input type="text" onChange={this.updateEmail}></input>
+                            <h3>Lösenord</h3>
+                            <input type="password" onChange={this.updatePassword}></input>
+                            <input type="submit" value="Logga in"></input>
+                        </form> 
+                    </div>
+                </div>  
+            </div>
+        </div>
+          );
+    }
+}
 export default Login;
--- src/views/Testytest.js
+++ src/views/Testytest.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Login.css'
 
 class Testytest extends Component {
     constructor(props) {
