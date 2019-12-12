9ab4880e32b05b1e2fa01c84aa1fbc6f82664a05
Peter Panduro <peter@kodifiera.se>
2019-12-12 16:57:50 +0100

    Fixed stuff

--- src/views/TestLogin.js
+++ src/views/TestLogin.js
@@ -12,15 +12,6 @@ class TestLogin extends Component {
             const response = await fetch(`${process.env.REACT_APP_API_HOST}/user/login`, {
                 method: 'POST',
                 headers: {
-<<<<<<< HEAD
-                    'email' : 'larpet-5@student.ltu.se',
-                    'password' : 'Root'
-                }
-            });
-            //this.setState({testlogin: await response.json()
-            //});
-        
-=======
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                     'email': 'larpet-5@student.ltu.se',
@@ -32,7 +23,6 @@ class TestLogin extends Component {
                     console.log(response.status);
                 })
             })
->>>>>>> 4a2eb2e96f9cc0f35bb5155116fe1fa2a26d802e
     }
   
     render() {
