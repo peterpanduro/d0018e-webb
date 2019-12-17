0ad8fde6bf1b4de93cf60ddc3a06fef198aaeb22
magdulator <magdaa.a@hotmail.com>
2019-12-17 17:26:32 +0100

    import beauty stars

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getComments } from '../functions/api'
-//import '../css/CommentList.css';
+import '../css/CommentList.css';
+import BeautyStars from 'beauty-stars';
 
 export default function CommentList(props) { 
 
@@ -23,12 +24,13 @@ export default function CommentList(props) {
 
   return (
     <div className="Comments">
-      <h2>Kommentarer</h2>
+      
       <ul>
         {comments.map(comment => (
           <li key={comment.ID}>
+            <p>Betyg: <BeautyStars value = {comment.Rating} activeColor= "yellow" size = "30px"/></p>
+            <h2>Kommentarer</h2>  
             <p>{comment.Name}</p>
-            <p>{comment.Rating}</p>
             <p>{comment.Opinion}</p>
           </li>
         ))}
