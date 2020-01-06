d99ca89519b595e413934c757b25097d60abef39
magdulator <magdaa.a@hotmail.com>
2020-01-06 14:10:55 +0100

    jodeliho

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -7,6 +7,7 @@ import '../css/ShoppingCart.css';
 export default function ShoppingCart() {
   
   const [cartState, cartDispatch] = useCart();
+  
 
   const buyButtonPressed = e => {
     e.preventDefault();
@@ -26,6 +27,22 @@ export default function ShoppingCart() {
     cartDispatch({type: 'set', content: {name, id, numberOfItems}});
   }
 
+  const increaseProducts = (e, item) => {
+    e.preventDefault();
+    const name = item.name;
+    const id = item.id;
+    const numberOfItems = item.numberOfItems + 1
+    cartDispatch({type: 'set', content: {name, id, numberOfItems}});
+  }
+
+  const decreaseProducts = (e, item) => {
+    e.preventDefault();
+    const name = item.name;
+    const id = item.id;
+    const numberOfItems = item.numberOfItems - 1;
+    cartDispatch({type: 'set', content: {name, id, numberOfItems}});
+  }
+
   return (
     <div className="ShoppingCart">
       <h1>Slutför din beställning</h1>
@@ -35,6 +52,9 @@ export default function ShoppingCart() {
           <div key={item.id}>
             <p>{item.numberOfItems} st. {item.name} </p>
             <p>DataMerchStore AB &trade;</p>
+            <input type='button' value='-' onClick={e=>decreaseProducts(e, item)}/>
+            <input type='text' value={item.numberOfItems} readOnly />
+            <input type='button' value='+' onClick={e=>increaseProducts(e, item)} />
             <button onClick={e => removeProduct(e, item)}>X</button>
           </div>      
         ))}
