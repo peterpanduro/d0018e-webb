3cedb0af7982426efcb01f0cfc7891578b570f1d
magdulator <magdaa.a@hotmail.com>
2020-01-06 15:40:02 +0100

    designy shitty

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,13 +1,54 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { getUser } from '../functions/api'
+import '../css/Account.css'
 
 
 export default function EditProfile() {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        checkCookie();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+
+    const checkCookie = () => {
+        const jwt = Cookies.get("jwt");
+        if (!jwt) {
+            window.location.assign("/");
+        } else {
+            getUser(jwt, (status, data) => {
+                if (status === 200) {
+                    setUser(data);
+                } else {
+                    console.log({status, data});
+                    alert("Check console");
+                }
+            })
+        }
+    }
+
     return (
         <div className = "EditProfile">
-            <h1>HEJ</h1>
-            <p>här kan man ju ändra uppgifter om man vill</p>
+            <h2>Mina sidor</h2>
+            <div className = "container">
+                <div className = "border">
+                    <p>Uppdatera profil</p>
+                </div>
+                <ul>
+                    <li><strong>Namn och Email-adress:</strong></li><br/>
+                    <li>{user.Name}</li><br/>
+                    <li>{user.Email}</li>
+                </ul>
+                <div className = "edit">
+                    <input type= 'text' placeholder='Namn'></input>
+                    <input type= 'text' placeholder='Email-adress'></input>
+                    <input type= 'password' placeholder='Lösenord'></input>
+                    <input type= 'submit' value ='Uppdatera profil'></input>
+                </div>
+            </div>
         </div>
-    );
+    )
 }
-
