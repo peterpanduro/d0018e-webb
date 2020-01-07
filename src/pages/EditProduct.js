ce6d763c2a90c63757a652b417b3ad32fa81eb69
hotpucko <stn.jonsson@gmail.com>
2020-01-07 14:36:15 +0100

    EditProduct cleanup and added category logic

--- src/pages/EditProduct.js
+++ src/pages/EditProduct.js
@@ -2,19 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Img from 'react-image'
 import '../css/Product.css';
 import CommentList from '../components/CommentList';
-import { getProduct } from '../functions/api'
+import { getProduct, getCategories } from '../functions/api'
 import Spinner from '../components/Spinner'
-import { useCartDispatch } from '../AppContext'
-
 
 export default function Product(props) {
-  
-  const cartDispatch = useCartDispatch();
-
   useEffect(() => {
     fetchProduct(props.match.params.product_id);
-  }, [props.match.params.product_id]);
-
+  }, [props.match.params.product_id]);  
   const [id, setId] = useState(0);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -22,30 +16,20 @@ export default function Product(props) {
   const [discountPrice, setDiscountPrice] = useState(0);
   const [imgURL, setImgURL] = useState("");
   const [imgCaption, setImgCaption] = useState("");
-
-  const [numberOfItems, setNumberOfItems] = useState(1);
-
-  const increaseNumberOfItems = e => {
-    e.preventDefault();
-    setNumberOfItems(numberOfItems+1);
-  }
-
-  const decreaseNumberOfItems = e => {
-    e.preventDefault();
-    setNumberOfItems(numberOfItems > 1 ? numberOfItems-1 : 1);
-  }
+  const [category, setCategory] = useState("");
 
   const fetchProduct = (id) => {
     getProduct(id, (status, data) => {
       if (status === 200) {
-        const {ID, Name, Description, Price, DiscountPrice, url, Caption} = data[0];
+        const {ID, Name, Description, Price, DiscountPrice, url, caption, Category} = data[0];
         setId(ID);
         setName(Name);
         setDescription(Description);
         setPrice(Price);
         setDiscountPrice(DiscountPrice);
         setImgURL(url);
-        setImgCaption(Caption);
+        setImgCaption(caption);
+        setCategory(Category);
       } else {
         console.log(data)
         alert(data)
@@ -53,9 +37,13 @@ export default function Product(props) {
     })
   }
 
-  const addItemInCart = e => {
-    e.preventDefault();
-    cartDispatch({type: 'add', content: {name, id, numberOfItems}});
+  const fetchCategories = () => {
+      getCategories((status, data) => {
+          if (status === 200) {
+              //const {ID, Name, Description} = data[0];
+              console.log(data);
+          }
+      })
   }
 
     return (
@@ -75,16 +63,18 @@ export default function Product(props) {
                   <input value={discountPrice} onChange={event => setDiscountPrice(event.target.value.replace(/\D/,''))}/>
                   <br/>
                   <label>Name: </label>
-                  <input type="text" name="name" value={name} maxlenth="20"></input>
+                  <input type="text" name="name" value={name} maxLength="20" onChange={e => setName(e.target.value)}></input>
                   <br/>
                   <label>Description: </label>
-                  <input type="text" name="description" value={description} maxlength="512"></input>
+                  <input type="text" name="description" value={description} maxLength="512" onChange={e => setName(e.target.value)}></input>
                   <br/>
                   <label>Image URL: </label>
-                  <input type="text" name="description" value={imgURL} maxlength="256"></input>
+                  <input type="text" name="imgURL" value={imgURL} maxLength="256" onChange={e => setName(e.target.value)}></input>
                   <br/>
                   <label>Image Caption: </label>
-                  <input type="text" name="description" value={imgCaption} maxlength="64"></input>
+                  <input type="text" name="imgCaption" value={imgCaption} maxLength="64" onChange={e => setName(e.target.value)}></input>
+                  <br/>
+                  <label>{category}</label>
               </form>
             </div>
           </div>
